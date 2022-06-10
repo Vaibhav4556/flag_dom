c869440ad4b3b082ad9028ce52926973efe0c025
Vaibhav4556 <107250286+Vaibhav4556@users.noreply.github.com>
2022-06-10 15:45:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+setTimeout(() => {
+    document.querySelector(".container").innerHTML = "<h1>10<h2>"
+    setTimeout(() => {
+        document.querySelector(".container").innerHTML = "<h1>9<h2>"
+        setTimeout(() => {
+            document.querySelector(".container").innerHTML = "<h1>8<h2>"
+            setTimeout(() => {
+                document.querySelector(".container").innerHTML = "<h1>7<h2>"
+                setTimeout(() => {
+                    document.querySelector(".container").innerHTML = "<h1>6<h2>"
+                    setTimeout(() => {
+                        document.querySelector(".container").innerHTML = "<h1>5<h2>"
+                        setTimeout(() => {
+                            document.querySelector(".container").innerHTML = "<h1>4<h2>"
+                            setTimeout(() => {
+                                document.querySelector(".container").innerHTML = "<h1>3<h2>"
+                                setTimeout(() => {
+                                    document.querySelector(".container").innerHTML = "<h1>2<h2>"
+                                    setTimeout(() => {
+                                        document.querySelector(".container").innerHTML = "<h1>1<h2>"
+                                        setTimeout(() => {
+                                            setTimeout(() => {
+                                                document.querySelector(".container").innerHTML = "<h1>Happy Indepenence Day <h2>"
+
+                                            }, 50)
+
+                                        }, 1000)
+
+                                    }, 1000)
+
+                                }, 1000)
+
+                            }, 1000)
+
+                        }, 1000)
+
+                    }, 1000)
+
+                }, 1000)
+
+            }, 1000)
+
+        }, 1000)
+
+    }, 1000)
+
+}, 1000)
